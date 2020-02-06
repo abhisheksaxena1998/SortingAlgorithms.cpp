7ea406e588e344fd7f18e66b9a46aec6fefe5482
Prince Gaur <48658242+proxy707@users.noreply.github.com>
2020-02-06 20:57:58 +0530

    Update bubble-sort.cpp

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -1,7 +1,7 @@
 #include "includeAll.h"
 using namespace std;
 
-// "Bubble Sort" Sorting Algorithm Time Complexity O(N^2).
+// "Bubble Sort" Sorting Algorithm Time Complexity O(N^2)
 void sort (int *listOfElements, int sizeOfList) {
 	for (int i = 0; i < sizeOfList - 1; ++i) {
 		for (int j = i+1; j < sizeOfList; ++j) 
